dcbb1a949ce4f7cd5f3456d0eb8d902f97618d21
brijender <brijender.v2@gmail.com>
2026-04-03 18:39:42 +0530

    Add PostgreSQL service to docker-compose and configure database connection in appsettings. Updated Program.cs to include DbContext and apply migrations.

--- YTSchool.Cms/YTSchool.Api.Cms/Program.cs
+++ YTSchool.Cms/YTSchool.Api.Cms/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
 using OpenTelemetry;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
+using YTSchool.Api.Cms.Database;
+using YTSchool.Api.Cms.Extensions;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +13,11 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("CmsConnection"),
+    npgsqlOptions => npgsqlOptions.MigrationsHistoryTable(HistoryRepository.DefaultTableName, Schemas.Application))
+    .UseSnakeCaseNamingConvention());
 
 builder.Services.AddOpenTelemetry()
     .ConfigureResource(resource => resource.AddService(builder.Environment.ApplicationName))
@@ -33,6 +42,8 @@ WebApplication app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    await app.ApplyMigrationsAsync();
 }
 
 app.UseHttpsRedirection();
